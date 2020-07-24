c8f4b6d3891448882e2f74aa468cdf8306ca9286
AshishChouhan85 <ashishchouhan2000.11@gmail.com>
2020-07-24 18:29:52 +0530

    blending added

--- Virtual controller.py
+++ Virtual controller.py
@@ -4,11 +4,10 @@ import math
 import serial
 
 
-
-
 def avg_background(roi):
     global r
     cv2.accumulateWeighted(roi,r,0.5)
+
 
 def segment(roi):
     global r
@@ -16,37 +15,25 @@ def segment(roi):
     bg=bg.astype("uint8")
     bg=cv2.GaussianBlur(bg,(7,7),0)
     roi = cv2.GaussianBlur(roi, (7,7), 0)
-
-
-
     diff=cv2.absdiff(roi,bg)
-
     diff=cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
     _,diff=cv2.threshold(diff,25,255,cv2.THRESH_BINARY)
     diff=cv2.dilate(diff,np.array([7,7],np.uint8),iterations=20)
-
     contours,_=cv2.findContours(diff,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
     return contours
+
 
 def controller(img,con,roi):
     global x,y
     hull = max(con, key=cv2.contourArea)
-
-    #left = tuple(hull[hull[:, :, 0].argmin()][0])
-    #bottom = tuple(hull[hull[:, :, 1].argmax()][0])
     top = tuple(hull[hull[:, :, 1].argmin()][0])
-    #right = tuple(hull[hull[:, :, 0].argmax()][0])
-
-
 
     img = cv2.circle(img, (x+top[0],y+top[1]), 25, (217,159,15), 5)
     img = cv2.circle(img, (x+top[0],y+top[1]), 40, (255, 255, 255),10)
     img = cv2.circle(img, (x + top[0], y + top[1]), 55, (217, 159, 15), 5)
-    #img = cv2.circle(img, (x + top[0], y + top[1]), 5, (255, 255, 255), 2)
-    #img[y:y+h,x:x+w,:]=cv2.addWeighted(img[y:y+h,x:x+w,:],0.5,roi,0.5,0)
-    #img = cv2.line(img, (x+w//2,y+h//2), (x+top[0],y+top[1]), (0, 0, 255), 3)
 
     return img,top
+
 
 def directions(roi,top,img):
     global w,h#,ser
@@ -81,7 +68,16 @@ def pwm(top):
     pwm=int(2.1*length)
     if(pwm>255):
         pwm=255
-    print(pwm)
+
+def blending(controller_bg,img):
+    global x,y,w,h,pad
+    error=20
+    controller_bg_drawn=controller_bg.copy()
+    controller_bg_drawn=img[y-pad:y+h+pad,x-pad:x+w+pad,:].copy()
+    blended_img=cv2.addWeighted(controller_bg,0.4,controller_bg_drawn,0.6,0)
+    img[y - pad:y + h + pad, x - pad:x + w + pad, :]=blended_img.copy()
+    return img
+
 
 
 
@@ -92,7 +88,7 @@ def pwm(top):
 cap=cv2.VideoCapture(0)
 _,f=cap.read()
 f=cv2.flip(f,1)
-x,y,w,h=320,115,250,250
+x,y,w,h,pad=320,115,250,250,100
 r=f[y:y+h,x:x+w,:].astype("float")
 no_of_frames=0
 z=1
@@ -100,9 +96,9 @@ while(z):
     ret,frame=cap.read()
     img=cv2.flip(frame,1)
     roi = img[y:y+h,x:x+w,:].copy()
-    img = cv2.circle(img, (x + w // 2, y + h // 2), h//2-10, (217,159,15), 3)
-    img = cv2.circle(img, (x + w // 2, y + h // 2), h // 2, (255, 255, 255), 10)
-    img = cv2.circle(img, (x + w // 2, y + h // 2), h // 2+10, (217,159,15), 3)
+    controller_bg=img[y-pad:y+h+pad,x-pad:x+w+pad,:].copy()
+    #cv2.imshow("e",controller_bg)
+
     if(no_of_frames<=60):
 
         avg_background(roi)
@@ -111,18 +107,24 @@ while(z):
         cv2.imshow("j", img)
     else:
         con=segment(roi)
+        img = cv2.circle(img, (x + w // 2, y + h // 2), h // 2 - 10, (217, 159, 15), 3)
+        img = cv2.circle(img, (x + w // 2, y + h // 2), h // 2, (255, 255, 255), 10)
+        img = cv2.circle(img, (x + w // 2, y + h // 2), h // 2 + 10, (217, 159, 15), 3)
 
         #img = cv2.circle(img, (x + w // 2, y + h // 2), 5, (255, 255, 255), 5)
         if(len(con)!=0):
             img,top=controller(img,con,roi)
+            img=blending(controller_bg, img)
             img=directions(roi,top,img)
             pwm(top)
+
 
             cv2.imshow("j", img)
         else:
             img = cv2.circle(img, (x + w // 2, y + h // 2), 25, (217, 159, 15), 5)
             img = cv2.circle(img, (x + w // 2, y + h // 2), 40, (255, 255, 255), 10)
             img = cv2.circle(img, (x + w // 2, y + h // 2), 55, (217, 159, 15), 5)
+            img = blending(controller_bg, img)
 
 
             cv2.imshow("j",img)
